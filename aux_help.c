fa3913f9c12436a9b3a3f1174fbb07fe47c4b976
oolufolabii <otitoola.olufolabi@gmail.com>
2022-06-15 21:08:41 +0100

    simple shell

--- aux_help.c
+++ aux_help.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * aux_help_evn - Help information for the builtin evn
+ * aux_help_env - Help information for the builtin evn
  * Retun: Always 0
  */
 
 void aux_help_env(void)
 {
-    char *help = "env: env [option] [name=value] [command [args]]\n\t";
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "Print the enviroment of the shell.\n";
-    write(STDOUT_FILENO, help, _strlen(help));
+char *help = "env: env [option] [name=value] [command [args]]\n\t";
+write(STDOUT_FILENO, help, _strlen(help));
+help = "Print the enviroment of the shell.\n";
+write(STDOUT_FILENO, help, _strlen(help));
 }
 
 /**
@@ -20,13 +20,13 @@ void aux_help_env(void)
 
 void aux_help_setenv(void)
 {
-    char *help = "setenv: setenv (const char *name, const char *value,";
+char *help = "setenv: setenv (const char *name, const char *value,";
 
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "int replace)\n\t";
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "Added a new definition to the environment\n";
-    write(STDOUT_FILENO, help, _strlen(help));
+write(STDOUT_FILENO, help, _strlen(help));
+help = "int replace)\n\t";
+write(STDOUT_FILENO, help, _strlen(help));
+help = "Added a new definition to the environment\n";
+write(STDOUT_FILENO, help, _strlen(help));
 }
 
 /**
@@ -36,11 +36,11 @@ void aux_help_setenv(void)
 
 void aux_help_unsetenv(void)
 {
-    char *help = "unsetenv: unsetenv (const char *name)\n\t";
+char *help = "unsetenv: unsetenv (const char *name)\n\t";
 
-    write(STDOUT_FILENO, help, _strlen(help));
-    hel = "Remove an entry completely from the environment\n";
-    write(STDOUT_FILENO, help, _strlen(help));
+write(STDOUT_FILENO, help, _strlen(help));
+hel = "Remove an entry completely from the environment\n";
+write(STDOUT_FILENO, help, _strlen(help));
 }
 
 /**
@@ -50,21 +50,21 @@ void aux_help_unsetenv(void)
 
 void aux_help_general(void)
 {
-    char *help = "^-^ bash, version 1.0(1)-release\n";
+char *help = "^-^ bash, version 1.0(1)-release\n";
 
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "These commands are defined internally.Type 'help' to see the list";
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "Type 'help name' to find out more about the function 'name'.\n\n ";
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = " alias: alias [name=['string']]\n cd: cd [-L|[-P [-e]] [-@]] ";
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "[dir]\nexit: exit [n]\n env: env [option] [name=value] [command ";
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "[args]\n setenv: setenv [variable] [value]\n unsetenv: ";
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "unsetenv [variable]\n";
-    write(STDOUT_FILENO, help, _strlen(help));
+write(STDOUT_FILENO, help, _strlen(help));
+help = "These commands are defined internally.Type 'help' to see the list";
+write(STDOUT_FILENO, help, _strlen(help));
+help = "Type 'help name' to find out more about the function 'name'.\n\n ";
+write(STDOUT_FILENO, help, _strlen(help));
+help = " alias: alias [name=['string']]\n cd: cd [-L|[-P [-e]] [-@]] ";
+write(STDOUT_FILENO, help, _strlen(help));
+help = "[dir]\nexit: exit [n]\n env: env [option] [name=value] [command ";
+write(STDOUT_FILENO, help, _strlen(help));
+help = "[args]\n setenv: setenv [variable] [value]\n unsetenv: ";
+write(STDOUT_FILENO, help, _strlen(help));
+help = "unsetenv [variable]\n";
+write(STDOUT_FILENO, help, _strlen(help));
 }
 
 
@@ -75,11 +75,11 @@ void aux_help_general(void)
 
 void aux_help_exit(void)
 {
-    char *help = "exit: exit [n]\n Exit shell.\n";
+char *help = "exit: exit [n]\n Exit shell.\n";
 
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "Exits the shell with a status of N. If N is ommited, the exit";
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "statusis that of the last command executed\n";
-    write(STDOUT_FILENO, help, _strlen(help));
+write(STDOUT_FILENO, help, _strlen(help));
+help = "Exits the shell with a status of N. If N is ommited, the exit";
+write(STDOUT_FILENO, help, _strlen(help));
+help = "statusis that of the last command executed\n";
+write(STDOUT_FILENO, help, _strlen(help));
 }
