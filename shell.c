53729fd088c0e915ebd86522ed0d0b1438d04881
oolufolabii <otitoola.olufolabi@gmail.com>
2022-06-15 21:58:49 +0100

    simple shell

--- shell.c
+++ shell.c
@@ -64,14 +64,14 @@ void convert_cmd(void)
 char *ptr;
 i = 0;
 ptr = strtok(cmd, " ");
-while(ptr != NULL)
+while (ptr != NULL)
 {
 argv[i] = ptr;
 i++;
 ptr = strtok(NULL, " ");
 }
 
-if(!strcmp("&", argv[i - 1]))
+if (!strcmp("&", argv[i - 1]))
 {
 argv[i - 1] = NULL;
 argv[i] = "&";
@@ -82,10 +82,11 @@ argv[i] = NULL;
 }
 }
 
-void log_handle(int sig){
+void log_handle(int sig)
+{
 FILE *pFile;
 pFile = fopen("log.txt", "a");
-if(pFile==NULL) perror("Error opening file.");
+if (pFile == NULL) perror("Error opening file.");
 else fprintf(pFile, "[LOG] child proccess terminated.\n");
 fclose(pFile);
 }
