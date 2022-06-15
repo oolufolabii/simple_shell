1d830f3ec7258b4e0db967fe90a600fbd024dc93
oolufolabii <otitoola.olufolabi@gmail.com>
2022-06-15 21:03:45 +0100

    simple shell

--- aux_error1.c
+++ aux_error1.c
@@ -43,34 +43,34 @@ return (error);
  */
 char *error_get_cd(data_shell *datash)
 {
-    int length, len_id;
-    char *error, *ver_str, *msg;
+int length, len_id;
+char *error, *ver_str, *msg;
 
-    ver_str = aux_itoa(datash->counter);
-    if (datash->args[1][0] == '-')
-    {
-        msg = ": Illegal option ";
-        len_id = 2;
-    }
-    else
-    {
-        msg = ": can't cd to ";
-        len_id = _strlen(datash->args[1]);
-    }
+ver_str = aux_itoa(datash->counter);
+if (datash->args[1][0] == '-')
+{
+msg = ": Illegal option ";
+len_id = 2;
+}
+else
+{
+msg = ": can't cd to ";
+len_id = _strlen(datash->args[1]);
+}
 
-    length = _strlen(datash->av[0]) + _strlen(datash->args[0]);
-    length += _strlen(ver_str) + _strlen(msg) + len_id + 5;
-    error = malloc(sizeof(char) * (length + 1));
+length = _strlen(datash->av[0]) + _strlen(datash->args[0]);
+length += _strlen(ver_str) + _strlen(msg) + len_id + 5;
+error = malloc(sizeof(char) * (length + 1));
 
-    if (error == 0)
-    {
-        free(ver_str);
-        return (NULL);
-    }
+if (error == 0)
+{
+free(ver_str);
+return (NULL);
+}
 
-    error = strcat_cd(datash, msg, error, ver_str);
-    free(ver_str);
-    return (error);
+error = strcat_cd(datash, msg, error, ver_str);
+free(ver_str);
+return (error);
 }
 
 /**
@@ -81,31 +81,31 @@ char *error_get_cd(data_shell *datash)
 
 char *error_not_found(data_shell *datash)
 {
-    int length;
-    char *error;
-    char *ver_str;
+int length;
+char *error;
+char *ver_str;
 
-    ver_str = aux_itoa(datash->counter);
-    length = _strlen(datash->av[0]) + _strlen(ver_str);
-    length += _strlen(datash->args[0]) + 16;
-    error = malloc(sizeof(char) * (length + 1));
-    if (error == 0)
-    {
-        free(error);
-        
-        free(ver_str);
+ver_str = aux_itoa(datash->counter);
+length = _strlen(datash->av[0]) + _strlen(ver_str);
+length += _strlen(datash->args[0]) + 16;
+error = malloc(sizeof(char) * (length + 1));
+if (error == 0)
+{
+free(error);
 
-        return (NULL);
-    }
-    _strcpy(error, datash->av[0]);
-    _strcat(error, ": ");
-    _strcat(error, ver_str);
-    _strcat(error, ": ");
-    _strcat(error, datash->args[0]);
-    _strcat(error, ": not fount\n");
-    _strcat(error, "\0");
-    free(ver_str);
-    return (error);
+free(ver_str);
+
+return (NULL);
+}
+_strcpy(error, datash->av[0]);
+_strcat(error, ": ");
+_strcat(error, ver_str);
+_strcat(error, ": ");
+_strcat(error, datash->args[0]);
+_strcat(error, ": not fount\n");
+_strcat(error, "\0");
+free(ver_str);
+return (error);
 }
 
 /**
@@ -116,28 +116,28 @@ char *error_not_found(data_shell *datash)
 
 char *error_exit_shell(data_shell *datash)
 {
-    int length;
-    char *error;
-    char *ver_str;
+int length;
+char *error;
+char *ver_str;
 
-    ver_str = aux_itoa(datash->counter);
-    length = _strlen(datash->av[0]) + _strlen(ver_str);
-    length += _strlen(datash->args[0]) + _strlen(datash->args[1]) + 23;
-    error = malloc(sizeof(char) * (length + 1));
-    if (error == 0)
-    {
-        free(ver_str);
-        return (NULL);
-    }
-    _strcpy(error, datash->av[0]);
-    _strcat(error, ": ");
-    _strcat(error, ver_str);
-    _strcat(error, ": ");
-    _strcat(error, datash->args[0]);
-    _strcat(error, ": Illegal number: ");
-    _strcat(error, datash->args[1]);
-    _strcat(error, "\n\0");
-    free(ver_str);
+ver_str = aux_itoa(datash->counter);
+length = _strlen(datash->av[0]) + _strlen(ver_str);
+length += _strlen(datash->args[0]) + _strlen(datash->args[1]) + 23;
+error = malloc(sizeof(char) * (length + 1));
+if (error == 0)
+{
+free(ver_str);
+return (NULL);
+}
+_strcpy(error, datash->av[0]);
+_strcat(error, ": ");
+_strcat(error, ver_str);
+_strcat(error, ": ");
+_strcat(error, datash->args[0]);
+_strcat(error, ": Illegal number: ");
+_strcat(error, datash->args[1]);
+_strcat(error, "\n\0");
+free(ver_str);
 
-    return (error);
+return (error);
 }
