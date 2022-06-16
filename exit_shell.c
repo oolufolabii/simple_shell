d67169d4749f7f14d9e78a10b4ee945126f763c0
Ade2002 <sikirusulaimon2002@gmail.com>
2022-06-16 05:56:38 +0100

    updating

--- exit_shell.c
+++ exit_shell.c
@@ -8,27 +8,27 @@
 
 int exit_shell(data_shell *datash)
 {
-unsigned int ustatus;
-int is_digit;
-int str_len;
-int big_number;
+	unsigned int ustatus;
+	int is_digit;
+	int str_len;
+	int big_number;
 
-if (datash->args[1] != NULL)
-{
-ustatus = _atoi(datash->args[1]);
-is_digit = _isdigit(datash->args[1]);
-str_len = _strlen(datash->args[1]);
-big_number = ustatus > (unsigned int)INT_MAX;
+	if (datash->args[1] != NULL)
+	{
+		ustatus = _atoi(datash->args[1]);
+		is_digit = _isdigit(datash->args[1]);
+		str_len = _strlen(datash->args[1]);
+		big_number = ustatus > (unsigned int)INT_MAX;
 
-if (!is_digit || str_len > 10 || big_number)
-{
-get_error(datash, 2);
-datash->status = 2;
-return (1);
-}
+		if (!is_digit || str_len > 10 || big_number)
+		{
+			get_error(datash, 2);
+			datash->status = 2;
+			return (1);
+		}
 
-datash->status = (ustatus % 256);
-}
+		datash->status = (ustatus % 256);
+	}
 
-return (0);
+	return (0);
 }
