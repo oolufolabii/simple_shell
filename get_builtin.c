79d33aabe99a89213ddcb52b0f5fad52af30631c
Ade2002 <sikirusulaimon2002@gmail.com>
2022-06-16 05:57:05 +0100

    updating

--- get_builtin.c
+++ get_builtin.c
@@ -8,23 +8,23 @@
 
 int (*get_builtin(char *cmd))(data_shell *)
 {
-builtin_t builtin[] = {
-{ "env", _env },
-{ "exit", exit_shell },
-{ "setenv", _setenv },
-{ "unsetenv", _unsetenv },
-{ "cd", cd_shell },
-{ "help", get_help },
-{ NULL, NULL }
-};
+	builtin_t builtin[] = {
+		{ "env", _env },
+		{ "exit", exit_shell },
+		{ "setenv", _setenv },
+		{ "unsetenv", _unsetenv },
+		{ "cd", cd_shell },
+		{ "help", get_help },
+		{ NULL, NULL }
+	};
 
-int i;
+	int i;
 
-for (i = 0; builtin[i].name; i++)
-{
-if (_strcmp(builtin[i].name, cmd) == 0)
-break;
-}
+	for (i = 0; builtin[i].name; i++)
+	{
+		if (_strcmp(builtin[i].name, cmd) == 0)
+			break;
+	}
 
-return (builtin[i].f);
+	return (builtin[i].f);
 }
