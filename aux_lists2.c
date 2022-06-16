40bc520842baa3011a60e1833b246bee9660a79b
Ade2002 <sikirusulaimon2002@gmail.com>
2022-06-16 05:47:33 +0100

    updating

--- aux_lists2.c
+++ aux_lists2.c
@@ -12,31 +12,31 @@
 
 r_var *add_rvar_node(r_var **head, int lvar, char *val, int lval)
 {
-r_var *new, *temp;
+	r_var *new, *temp;
 
-new = malloc(sizeof(r_var));
-if (new == NULL)
-return (NULL);
+	new = malloc(sizeof(r_var));
+	if (new == NULL)
+		return (NULL);
 
-new->len_var = lvar;
-new->val = val;
-new->len_val = lval;
+	new->len_var = lvar;
+	new->val = val;
+	new->len_val = lval;
 
-new->next = NULL;
-temp = *head;
+	new->next = NULL;
+	temp = *head;
 
-if (temp == NULL)
-{
-*head = new;
-}
-else
-{
-while (temp->next != NULL)
-temp = temp->next;
-temp->next = new;
-}
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
 
-return (*head);
+	return (*head);
 }
 
 /**
@@ -47,17 +47,17 @@ return (*head);
 
 void free_rvar_list(r_var **head)
 {
-r_var *temp;
-r_var *curr;
+	r_var *temp;
+	r_var *curr;
 
-if (head != NULL)
-{
-curr = *head;
-while ((temp = curr) != NULL)
-{
-curr = curr->next;
-free(temp);
-}
-*head = NULL;
-}
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
 }
